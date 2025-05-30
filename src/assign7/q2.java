0aef24f5d48b3de0b31499d14d337e1750929b13
Sritish <sritishkumargouda@gmail.com>
2025-05-30 11:48:31 +0530

    Adding Assign 7 files (mid)

--- src/assign7/q2.java
+++ src/assign7/q2.java
@@ -1,6 +1,9 @@
 package assign7;
 
-interface Calculator{
+import java.util.ArrayList;
+import java.util.Arrays;
+
+interface Calculator {
     public double calculate(double a, double b);
 }
 
@@ -10,8 +13,32 @@ public class q2 {
         double num1 = 10.1d;
         double num2 = 10.1d;
 
-        
+        Calculator add = (x, y) -> x + y;
+        Calculator sub = (x, y) -> x - y;
+        Calculator mul = (x, y) -> x * y;
+        Calculator div = (x, y) -> {
+            if (y != 0) {
+                return (x / y);
+            }
+            return 0;
+        };
+
+        System.out.println(add.calculate(num1, num2));
+        System.out.println(mul.calculate(num1, num2));
+
+        // Extra ------------------------------------------------
+        // No neeed
+        ArrayList<Calculator> op = new ArrayList<>();
+        op.add(add);
+        op.add(sub);
+        op.add(mul);
+        op.add(div);
+
+        op.forEach((ele) -> {
+            double var = ele.calculate(num1, num2);
+            System.out.println(var);
+        });
 
     }
-    
+
 }
---
+++ src/assign7/q4.java
@@ -0,0 +1,5 @@
+package assign7;
+
+public class q4 {
+
+}
