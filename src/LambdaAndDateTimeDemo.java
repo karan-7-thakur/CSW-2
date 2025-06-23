b5136f91fdc4221be53f802e25abd4f6c2afa48c
first project-demo <thakurkaran.1197@gmail.com>
2025-06-23 20:16:12 +0530

    hehehehehehehehhehhehehehe

--- src/LambdaAndDateTimeDemo.java
+++ src/LambdaAndDateTimeDemo.java
@@ -1,7 +1,7 @@
 import java.time.*;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
 import java.util.function.Consumer;
+
 
 @FunctionalInterface
 interface Greeting {
